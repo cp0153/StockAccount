c213fe3e2cc82bde15ff8754f54e5f691c77b00c
cp0153 <christopher.hale.pearce@gmail.com>
2015-06-23 16:42:34 -0400

    branch test

--- StockAccount/StockMarketTest.cpp
+++ StockAccount/StockMarketTest.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <map>
 #include "StockMarket.h"
 
 int main()
 {
-	StockMarket test;
+	/*StockMarket test;
 	test.displayStockMarket();
 	std::cout << "\n";
 	test.updateStockMarket();
@@ -13,6 +14,15 @@ int main()
 	test.displayStockMarket();
 	std::cout << "\n";
 	test.updateStockMarket();
-	test.displayStockMarket();
+	test.displayStockMarket();*/
+
+	std::map<int, int> m;
+	m.insert(std::pair<int, int>(1, 100));
+	m.insert(std::pair<int, int>(2, 200));
+	//std::vector<int> v;
+	for (std::map<int, int>::iterator it = m.begin(); it != m.end(); ++it) {
+		//v.push_back(it->first);
+		std::cout << it->first << "\n";
+	}
 	return 0;
 }
