d1c41b7b68d5e2a874b1c59216080e1ad04518a4
cp0153 <christopher.hale.pearce@gmail.com>
2015-07-03 22:01:34 -0400

    uncommented main

--- StockAccount/main.cpp
+++ StockAccount/main.cpp
@@ -1,11 +1,11 @@
-//#include "BrokerageAccount.h"
-//#include <iomanip>
-//int main()
-//{
-//	std::cout << std::fixed;
-//	std::cout << std::setprecision(2);
-//	brokerageAccount brokerage_account;
-//	brokerage_account.mainMenu();
-//
-//	return 0;
-//}
+#include "BrokerageAccount.h"
+#include <iomanip>
+int main()
+{
+	std::cout << std::fixed;
+	std::cout << std::setprecision(2);
+	brokerageAccount brokerage_account;
+	brokerage_account.mainMenu();
+
+	return 0;
+}
